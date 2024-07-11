c8852481ac8d60866185a1aedc3a1779bcac58d8
Norbert Kohari <106980094+KohariiDev@users.noreply.github.com>
2024-07-11 17:43:07 +0200

    disable sentry

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Model from './components/Model';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
-
-import * as Sentry from '@sentry/react';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -19,7 +17,7 @@ const App = () => {
       <HowItWorks />
       <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default Sentry.withProfiler(App);
+export default App;
