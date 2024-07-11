b7558479f613d2f260abcd08a371ef0651351f11
Norbert Kohari <106980094+KohariiDev@users.noreply.github.com>
2024-07-11 17:51:03 +0200

    deleted Sentry

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-x9",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "kod-i1",
-    project: "javascript-react"
-  })],
-
+  plugins: [react()],
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
